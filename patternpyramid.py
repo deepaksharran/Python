eac67a902f54c46fc83ea4beaab2eb028248f66f
deepaksharran <42145768+deepaksharran@users.noreply.github.com>
2019-08-06 12:07:25 +0530

    Add files via upload

---
+++ patternpyramid.py
@@ -0,0 +1,8 @@
+def pattern(n):
+    for i in range(n):
+        for k in range(n,i,-1):
+            print(" ",end="")
+        for j in range(i+1):
+            print("*",end=" ")
+        print("\r")
+pattern(5)
