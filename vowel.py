2f86a9789854f39151452e85d22e0855c0e5345a
deepaksharran <42145768+deepaksharran@users.noreply.github.com>
2019-08-07 22:08:30 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,5 @@
+let=input("enter a letter:")
+if let=='A' or let=='a' or let=='E' or let=='e' or let=='I' or let=='i' or let=='O' or let=='o' or let=='U' or let=='u':
+    print(let," is a vowel")
+else:
+    print(let,"is not a vowel")
